aefae114d347c29db5ba1bff5f6fb2d79d163a8e
Abigail Addae <abigail2addae@gmail.com>
2022-09-14 22:51:45 -0400

    add more levels

--- client/src/components/Reading/Reading.jsx
+++ client/src/components/Reading/Reading.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import "./Reading.css"
 
-import Flashcard from "../Flashcards/Flashcard";
+import FlashcardList from "../Flashcards/FlashcardList";
 
 const MODE = {
   viewFlashcards: "VIEW_FLASHCARDS", 
@@ -16,7 +16,7 @@ export default function Reading() {
   const showFlashcards = () => {
     setMode(MODE.viewFlashcards);
   }
-  
+
   const quitRound = () => {
     setMode(MODE.viewMap);
   }
@@ -29,13 +29,29 @@ export default function Reading() {
           <h2>Reading Practice!</h2>
           <h3>HSK 1</h3>
           <ul>
-            <li>
+            <li className="d-flex justify-content-center">
             <section className="lvl-section border rounded-circle d-flex flex-column justify-content-center">
               <h4>Level 1</h4>
               <button onClick={showFlashcards} type="button" className="btn btn-success align-self-center">Start</button>
               <p>Score:</p>
             </section>
             </li>
+            <div className="d-flex justify-content-around">
+              <li className="mx-2">
+              <section className="lvl-section border rounded-circle d-flex flex-column justify-content-center">
+                <h4>Level 2</h4>
+                <button onClick={showFlashcards} type="button" className="btn btn-success align-self-center">Start</button>
+                <p>Score:</p>
+              </section>
+              </li>
+              <li className="mx-2">
+              <section className="lvl-section border rounded-circle d-flex flex-column justify-content-center">
+                <h4>Level 3</h4>
+                <button onClick={showFlashcards} type="button" className="btn btn-success align-self-center">Start</button>
+                <p>Score:</p>
+              </section>
+              </li>
+            </div>
           </ul>
         </>
       }
@@ -43,7 +59,7 @@ export default function Reading() {
         mode === MODE.viewFlashcards &&
 
         <section className="flashcards d-flex">
-          <Flashcard quitRound={quitRound}/>
+          <FlashcardList quitRound={quitRound}/>
         </section>
       }
     </section>
