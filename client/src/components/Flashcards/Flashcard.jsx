ebfa187c05335e9f2944bf79cd4a791d26a9d53c
Abigail Addae <abigail2addae@gmail.com>
2022-09-14 22:51:13 -0400

    update how prop is read

--- client/src/components/Flashcards/Flashcard.jsx
+++ client/src/components/Flashcards/Flashcard.jsx
@@ -9,15 +9,15 @@ export default function Flashcard({quitRound, flashcard}) {
   }
 
   return (
-    <div id={flashcard[1].id} className="flip-card">
+    <div id={flashcard.id} className="flip-card">
       <div className="flip-card-inner">
         <div className="card flip-card-front">
           <button onClick={quitRound} type="button" className="btn-close position-absolute top-0 end-0" aria-label="Close"></button>
           <img src="logo57.png" className="card-img-top" alt="logo"/>
           <div className="card-body">
             <h5 className="card-title">Question</h5>
-            <p className="card-text fs-1">{flashcard[1].character} <i className="bi bi-volume-up"></i> </p>
-            <button onClick={()=>flipFlashcard(flashcard[1].id)} className="btn btn-primary">Flip</button>
+            <p className="card-text fs-1">{flashcard.character} <i className="bi bi-volume-up"></i> </p>
+            <button onClick={()=>flipFlashcard(flashcard.id)} className="btn btn-primary">Flip</button>
             <button className="btn btn-primary disabled">Star</button>
           </div>
         </div>
@@ -26,13 +26,13 @@ export default function Flashcard({quitRound, flashcard}) {
           <img src="logo57.png" className="card-img-top" alt="logo"/>
           <div className="card-body">
             <h5 className="card-title">Answer</h5>
-            <p className="card-text fs-1">{flashcard[1].pinyin} <i className="bi bi-volume-up"></i></p>
+            <p className="card-text fs-1">{flashcard.pinyin} <i className="bi bi-volume-up"></i></p>
             <div className="d-flex justify-content-around">
               <button className="btn btn-success">Right</button>
               <button className="btn btn-danger">Wrong</button>
             </div>
             <div className="d-flex justify-content-around">
-              <button onClick={()=>flipFlashcard(flashcard[1].id)} className="btn btn-primary">Flip</button>
+              <button onClick={()=>flipFlashcard(flashcard.id)} className="btn btn-primary">Flip</button>
               <button className="btn btn-primary disabled">Star</button>
               <button className="btn btn-primary disabled">Examples</button>
             </div>
